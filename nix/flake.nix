e88a3bd3234f8d675fe4ca4a9e012a102b9466aa
Yomi Ikuru <captyomjnr@gmail.com>
2025-05-16 21:37:05 +0100

    feat: add dev environment

---
+++ nix/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Yomi's blog development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    overlays = [];
+
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit overlays system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          astro-language-server
+        ];
+      };
+    });
+  };
+}
