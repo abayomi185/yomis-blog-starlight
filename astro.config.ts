7afb3d03e29a3a014420dfbc278e573bb205817f
Yomi Ikuru <captyomjnr@gmail.com>
2025-10-03 12:46:45 +0100

    feat: update astro config

--- astro.config.ts
+++ astro.config.ts
@@ -1,26 +1,45 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightBlog from 'starlight-blog';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Yomi's Blog",
+      plugins: [starlightBlog()],
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/abayomi185'
+        },
+        {
+          icon: 'instagram',
+          label: 'Instagram',
+          href: 'https://instagram.com/yomi185'
+        },
+        {
+          icon: 'linkedin',
+          label: 'LinkedIn',
+          href: 'https://www.linkedin.com/in/yomi-ikuru'
+        }
+      ],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' }
+          ]
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' }
+        }
+      ]
+    })
+  ],
+  site: 'https://yomis.blog'
 });
